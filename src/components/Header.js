e623695cdfe71b4a5bded9f29ac405ac32131a91
GangbacOl <gangyeonjo336@gmail.com>
2020-10-14 19:19:17 +0900

    Update: Header 컴포넌트 업데이트.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,44 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import TodoContext from '../contexts/TodoContext';
 import { API_KEY } from '../config/config';
 
 const HeaderWrapper = styled.div`
-    border: 1px solid black;
-    padding: 10px;
-    margin: 10px;
-    font-weight: 100;
-    font-size: 2rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin-right: 15px;
+    text-align: right;
+    font-size: 1.5rem;
+    color: white;
+`;
+
+const WeatherContentWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    color: white;
+`;
+
+const WeatherWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const SpanWrapper = styled.p`
+    font-size: 0.9rem;
+    margin: 0 5px 0 0;
 `;
 
 const Header = () => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [apiData, setApiData] = useState({});
-    const { actions } = useContext(TodoContext);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(async (position) => {
             const { data } = await axios.get(
                 `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${API_KEY}`
             );
-            console.log(data);
             setApiData(data);
             setIsLoaded(true);
         });
@@ -30,10 +46,18 @@ const Header = () => {
     return (
         <HeaderWrapper>
             {isLoaded ? (
-                <div className="weather">
-                    <img src={`http://openweathermap.org/img/wn/${apiData.weather[0].icon}.png`} alt="" />
-                    <span>{apiData.main.temp}도</span>
-                </div>
+                <WeatherWrapper>
+                    <WeatherContentWrapper>
+                        <img
+                            src={`http://openweathermap.org/img/wn/${apiData.weather[0].icon}.png`}
+                            alt=""
+                            stlye={{ width: 30 }}
+                        />
+                        <span>{parseInt(apiData.main.temp)}°</span>
+                    </WeatherContentWrapper>
+                    <SpanWrapper>{apiData.name}</SpanWrapper>
+                    {/* {apiData.weather[0].main} */}
+                </WeatherWrapper>
             ) : null}
         </HeaderWrapper>
     );
