96f8335a101303382a3be3618194e87fed5a22fa
GangbacOl <gangyeonjo336@gmail.com>
2020-10-14 19:15:09 +0900

    Update: styled-component를 이용한 스타일링, 시간 관련 로직 추가.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,20 +1,45 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const GreetingWrapper = styled.div`
-    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const H2Wrapper = styled.h2`
+    margin: 10px;
+    font-size: 10rem;
+    font-weight: bold;
+`;
+
+const PWrapper = styled.p`
+    text-align: center;
+    font-size: 2.6rem;
+    font-weight: bold;
 `;
 
 const Greeting = () => {
     const [username, setUsername] = useState('');
+    const [currentTime, setCurrentTime] = useState('');
+    const getCurrentTime = () => {
+        const h = new Date().getHours() < 10 ? '0' + new Date().getHours() : new Date().getHours();
+        const m = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes();
+        const s = new Date().getSeconds() < 10 ? '0' + new Date().getSeconds() : new Date().getSeconds();
+        return h + ':' + m + ':' + s;
+    };
     useEffect(() => {
         setUsername(localStorage.getItem('username'));
+        setInterval(() => {
+            setCurrentTime(getCurrentTime());
+        }, 1000);
     }, []);
     return (
         <GreetingWrapper>
-            <div>안녕하세요. {username}</div>
+            <H2Wrapper>{currentTime}</H2Wrapper>
+            <PWrapper>안녕하세요, {username}님</PWrapper>
         </GreetingWrapper>
     );
 };
 
-export default Greeting;
+export default React.memo(Greeting);
