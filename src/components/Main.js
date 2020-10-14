f4c9a8c9a85518f6b6fe4136d927ebec7fa2bd2a
GangbacOl <gangyeonjo336@gmail.com>
2020-10-14 19:16:04 +0900

    Update: Main 컴포넌트 업데이트.

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,34 +5,58 @@ import Greeting from './Greeting';
 import First from './First';
 
 const MainWrapper = styled.div`
-    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     padding: 10px;
     margin: 10px;
-    font-weight: 100;
+    font-size: 2rem;
+    color: white;
+`;
+
+const GreetingWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const InputWrapper = styled.input`
+    all: unset;
+    border-bottom: 3px solid white;
+    width: 500px;
+    min-width: 400px;
+    text-align: center;
     font-size: 2rem;
 `;
 
 const Main = () => {
     const [inputText, setInputText] = useState('');
-    const [isLogged, setIsLogged] = useState(null);
+    const [isLogged, setIsLogged] = useState(false);
     const value = useContext(TodoContext);
     useEffect(() => {
         setIsLogged(localStorage.getItem('isLogged'));
     }, []);
     return (
         <MainWrapper>
-            {isLogged ? <Greeting /> : <First />}
-            <input
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        value.actions.createList(inputText);
-                        setInputText('');
-                    }
-                }}
-            />
+            {isLogged ? (
+                <GreetingWrapper>
+                    <Greeting />
+                    <InputWrapper
+                        type="text"
+                        value={inputText}
+                        onChange={(e) => setInputText(e.target.value)}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                value.actions.createList(inputText);
+                                setInputText('');
+                            }
+                        }}
+                    />
+                </GreetingWrapper>
+            ) : (
+                <First />
+            )}
         </MainWrapper>
     );
 };
